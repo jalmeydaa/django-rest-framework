0c240b1860065f95481788e093a624fc896643cb
Juan Carlos <juanqui.912@gmail.com>
2018-04-16 10:40:16 -0500

    [UPDATE] - Serializers Snippet - create and update

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from snippets.models import Snippet, SnippetDetail,\
 LANGUAGE_CHOICES, STYLE_CHOICES
+from rest_framework_bulk import (
+    BulkListSerializer,
+    BulkSerializerMixin,
+    ListBulkCreateUpdateDestroyAPIView,
+)
+
 
 
 class SnippetSerializer(serializers.Serializer):
@@ -30,39 +36,13 @@ class SnippetSerializer(serializers.Serializer):
         return instance
 
 
-class SnippetDetailListSerializer(serializers.ListSerializer):
-
-    def update(self, instance, validated_data):
-        # Maps for id->instance and id->data item.
-        snippet_detail_mapping = {snippet_detail.id: snippet_detail for snippet_detail in instance}
-        data_mapping = {item['id']: item for item in validated_data}
-        print(data_mapping)
-
-        # Perform creations and updates.
-        ret = []
-        for snippet_detail_id, data in data_mapping.items():
-            snippet_detail = snippet_detail_mapping.get(snippet_detail_id, None)
-            if snippet_detail is None:
-                ret.append(self.child.create(data))
-            else:
-                ret.append(self.child.update(snippet_detail, data))
-
-        # Perform deletions.
-        for snippet_detail_id, snippet_detail in snippet_detail_mapping.items():
-            if snippet_detail_id not in data_mapping:
-                snippet_detail.delete()
-
-        return ret
-
-
 class SnippetDetailSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
+    id = serializers.IntegerField(required=False, allow_null=True)
     code = serializers.CharField(required=True)
 
     class Meta:
         model = SnippetDetail
         fields = '__all__'
-        list_serializer_class = SnippetDetailListSerializer
 
 
 class SnippetModelSerializer(serializers.ModelSerializer):
@@ -73,44 +53,34 @@ class SnippetModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        snippet_data = {
-            'title': validated_data.get('title'),
-            'code': validated_data.get('code'),
-            'linenos': validated_data.get('linenos'),
-            'language': validated_data.get('language'),
-            'style': validated_data.get('style')
-        }
-        snippet = Snippet.objects.create(**snippet_data)
+        details = validated_data.pop('details')
+        snippet = Snippet.objects.create(**validated_data)
 
-        details = validated_data.get('details')
-
-        for d in details:
-            d['snippet'] = snippet.id
-
-        serializer_details = SnippetDetailSerializer(data=details, many=True)
-        if serializer_details.is_valid():
-            serializer_details.save()
-
+        for detail in details:
+            detail_id = detail.pop('id', None)
+            d, created = SnippetDetail.objects.update_or_create(
+                id=detail_id,
+                defaults={
+                    'snippet_id': snippet.id,
+                    'name': detail.get('name'),
+                    'code': detail.get('code')
+                })
         return snippet
 
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.lonenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-
-        details = validated_data.get('details')
-        for d in details:
-            d['snippet'] = instance.id
-
-
-        serializer_details = SnippetDetailSerializer(
-            instance=instance.details.all(), data=details, many=True
-        )
-        if serializer_details.is_valid():
-            serializer_details.save()
+        details = validated_data.pop('details')
+        ok = Snippet.objects.update(**validated_data)
+        for detail in details:
+            detail_id = detail.pop('id', None)
+            d, created = SnippetDetail.objects.update_or_create(
+                id=detail_id,
+                defaults={
+                    'snippet_id': instance.id,
+                    'name': detail.get('name'),
+                    'code': detail.get('code')
+                }
+            )
+            print(d, created)
 
         return instance
